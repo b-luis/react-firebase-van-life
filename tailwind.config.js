ac29b53f9201c3a7df29143eb11780dcbe0dfb72
b-luis <bscarletluis@gmail.com>
2023-09-15 19:05:33 +0800

    van life - initial files

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        default: "#4D4D4D",
+        active: "#161616",
+      },
+    },
+  },
+  plugins: [],
+};
