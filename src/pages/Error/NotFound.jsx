c7eec143d029effd84e9c652b06381cba852ea84
b-luis <bscarletluis@gmail.com>
2023-09-22 19:01:15 +0800

    feat: add error page

--- src/pages/Error/NotFound.jsx
+++ src/pages/Error/NotFound.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const styles = {
+  notFoundDiv:
+    "px-9 mx-auto flex flex-col justify-center lg:max-w-[900px] lg:h-[650px] h-[400px] lg:px-0",
+  notFoundText: "py-4 font-semibold text-4xl",
+  notFoundLink:
+    "bg-[#161616] text-white py-3 w-[50%] text-center md:w-[20%] font-bold hover:bg-[#804a1c]",
+};
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>Van item not found</h1>
+    <div className={styles.notFoundDiv}>
+      <h1 className={styles.notFoundText}>
+        Sorry, the page you were looking for was not found. But, don't fret! You
+        can return home. (❁´◡`❁)
+      </h1>
+      <Link to="/" className={styles.notFoundLink}>
+        Return to Home
+      </Link>
     </div>
   );
 };
